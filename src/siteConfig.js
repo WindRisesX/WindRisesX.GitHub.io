33b6415f6c44a5d6d8a9db44d0620f9123339e85
x <x@xdeMacBook-Pro.local>
2018-05-07 15:01:21 +0800

    init

---
+++ src/siteConfig.js
@@ -0,0 +1,17 @@
+module.exports = {
+  /**
+   * SEO
+   */
+  blogTitle: "WindRises's Blog",
+  blogDescription: "WindRises's Blog",
+  blogAuthor: 'WindRises',
+  blogKeywords: 'WindRises, WindRises\'Blog, vue, vue-blog, GitHub-page, 博客',
+  favicon: './src/assets/favicon.png',
+
+  /**
+   * GitHub
+   */
+  repo: 'UnableRegister/blog-article',
+  path: '',
+  branch: 'master',
+}
